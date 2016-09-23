72d4a56b895ed02b8e23b56a58174c053e154098
MAILLOUX Carl <cmailloux@adetel.com>
2016-09-22 22:46:52 -0400

    Add helper class to act a notification sender

---
+++ GROUND.PLATEFORM/NUnittest/DataPackageTests/Stubs/NotificationInfo.cs
@@ -0,0 +1,100 @@
+﻿//---------------------------------------------------------------------------------------------------
+// <copyright file="NotificationInfo.cs" company="Alstom">
+//          (c) Copyright ALSTOM 2016.  All rights reserved.
+//
+//          This computer program may not be used, copied, distributed, corrected, modified, translated,
+//          transmitted or assigned without the prior written authorization of ALSTOM.
+// </copyright>
+//---------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PIS.Ground.GroundCore.AppGround;
+using System.Globalization;
+
+namespace DataPackageTests.Stubs
+{
+    /// <summary>
+    /// Describe a notification.
+    /// </summary>
+    /// <remarks>This object is immutable</remarks>
+    class NotificationInfo
+    {
+        // String that allow cleaning parameter string.
+        private const string BEGIN_INSTR = "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <string>";
+        private const string END_INSTR = "</string>\r\n</ArrayOfString>";
+        private const string COMMA_INSTR = "</string>\r\n  <string>";
+
+        /// <summary>
+        /// Gets the notification identifier.
+        /// </summary>
+        public NotificationIdEnum Id { get; private set; }
+
+        /// <summary>
+        /// Gets the notification request identifier.
+        /// </summary>
+        public Guid RequestId { get; private set; }
+
+        /// <summary>
+        /// Gets the parameter associated to the notification.
+        /// </summary>
+        public string Parameter { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotificationInfo"/> class.
+        /// </summary>
+        /// <param name="id">The notification identifier.</param>
+        public NotificationInfo(NotificationIdEnum id)
+            : this(id, Guid.Empty, null)
+        {
+            /* No logic body */
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotificationInfo"/> class.
+        /// </summary>
+        /// <param name="id">The notification identifier.</param>
+        /// <param name="requestId">The request identifier.</param>
+        public NotificationInfo(NotificationIdEnum id, Guid requestId)
+            : this(id, requestId, null)
+        {
+            /* No logic body */
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotificationInfo"/> class.
+        /// </summary>
+        /// <param name="id">The notification identifier.</param>
+        /// <param name="requestId">The request identifier.</param>
+        /// <param name="parameter">Additional parameter</param>
+        public NotificationInfo(NotificationIdEnum id, Guid requestId, string parameter)
+        {
+            Id = id;
+            RequestId = requestId;
+            Parameter = parameter;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return (string.IsNullOrEmpty(Parameter)) ?
+                string.Format(CultureInfo.InvariantCulture, "{0,-50} @ {1}", Id, RequestId) :
+                string.Format(CultureInfo.InvariantCulture, "{0,-50} @ {1} [{2}]", Id, RequestId, GetParameterAsCommaDelimitedString());
+        }
+
+        /// <summary>
+        /// Clear the parameter string to display the content as a comma delimited string.
+        /// </summary>
+        /// <returns>Parameter values separated with comma character.</returns>
+        public string GetParameterAsCommaDelimitedString()
+        {
+            return Parameter.Replace(BEGIN_INSTR, string.Empty).Replace(END_INSTR, string.Empty).Replace(COMMA_INSTR, ", ");
+        }
+    }
+}
---
+++ GROUND.PLATEFORM/NUnittest/DataPackageTests/Stubs/NotificationSenderStub.cs
@@ -0,0 +1,153 @@
+﻿//---------------------------------------------------------------------------------------------------
+// <copyright file="NotificationSenderStub.cs" company="Alstom">
+//          (c) Copyright ALSTOM 2016.  All rights reserved.
+//
+//          This computer program may not be used, copied, distributed, corrected, modified, translated,
+//          transmitted or assigned without the prior written authorization of ALSTOM.
+// </copyright>
+//---------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PIS.Ground.Core.Common;
+
+namespace DataPackageTests.Stubs
+{
+    /// <summary>
+    /// Implementation of INotificationSender interface that allow to display notification on the console and
+    /// record notification send by datapackage service.
+    /// </summary>
+    /// <seealso cref="PIS.Ground.Core.Common.INotificationSender" />
+    class NotificationSenderStub : INotificationSender
+    {
+        #region Fields
+
+        private object _lock = new object();
+
+        private List<NotificationInfo> _notifications = new List<NotificationInfo>(100);
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a value indicating whether notifications are output on the console.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> to output notifications; otherwise, <c>false</c>.
+        /// </value>
+        bool DisplayNotifications { get; set; }
+
+        #endregion
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotificationSenderStub"/> class.
+        /// </summary>
+        /// <param name="displayNotifications">Indicates if notification shall be output on console.</param>
+        public NotificationSenderStub(bool displayNotifications)
+        {
+            DisplayNotifications = displayNotifications;
+        }
+        #endregion
+
+        /// <summary>
+        /// Gets the notifications.
+        /// </summary>
+        /// <returns>The notifications</returns>
+        public List<NotificationInfo> GetNotifications()
+        {
+            lock (_lock)
+            {
+                return new List<NotificationInfo>(_notifications);
+            }
+        }
+
+        /// <summary>
+        /// Gets the notifications that match the specified request identifier.
+        /// </summary>
+        /// <param name="requestId">The request identifier.</param>
+        /// <returns>The notifications that match</returns>
+        public List<NotificationInfo> GetNotifications(Guid requestId)
+        {
+            lock (_lock)
+            {
+                return _notifications.Where(n => n.RequestId == requestId).ToList();
+            }
+        }
+
+        #region INotificationSender Members
+
+        /// <summary>
+        /// Sends the notification.
+        /// </summary>
+        /// <param name="notificationId">The notification identifier.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="requestId">The request identifier.</param>
+        public void SendNotification(PIS.Ground.GroundCore.AppGround.NotificationIdEnum notificationId, string parameter, Guid requestId)
+        {
+            Add(new NotificationInfo(notificationId, requestId, parameter));
+        }
+
+        /// <summary>
+        /// Sends the notification.
+        /// </summary>
+        /// <param name="notificationId">The notification identifier.</param>
+        /// <param name="parameter">The parametr.</param>
+        public void SendNotification(PIS.Ground.GroundCore.AppGround.NotificationIdEnum notificationId, string parameter)
+        {
+            Add(new NotificationInfo(notificationId, Guid.Empty, parameter));
+        }
+
+        /// <summary>
+        /// Sends the notification.
+        /// </summary>
+        /// <param name="notificationId">The notification identifier.</param>
+        /// <param name="requestId">The request identifier.</param>
+        public void SendNotification(PIS.Ground.GroundCore.AppGround.NotificationIdEnum notificationId, Guid requestId)
+        {
+            Add(new NotificationInfo(notificationId, requestId));
+        }
+
+        /// <summary>
+        /// Sends the notification.
+        /// </summary>
+        /// <param name="notificationId">The notification identifier.</param>
+        public void SendNotification(PIS.Ground.GroundCore.AppGround.NotificationIdEnum notificationId)
+        {
+            Add(new NotificationInfo(notificationId));
+        }
+
+        public void SendT2GServerConnectionStatus(bool status)
+        {
+            if (DisplayNotifications)
+            {
+                Console.Out.WriteLine("  GROUND NOTIFICATION: T2GServerConnectionStatus({0})", status);
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Adds the specified notification.
+        /// </summary>
+        /// <param name="notification">The notification.</param>
+        private void Add(NotificationInfo notification)
+        {
+            lock (_lock)
+            {
+                _notifications.Add(notification);
+            }
+
+            if (DisplayNotifications)
+            {
+                Console.Out.WriteLine("  GROUND NOTIFICATION: {0}", notification);
+            }
+        }
+        #endregion 
+    }
+}
